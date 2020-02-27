b04da8560c9b114bc5dec8f18bdc71a93c8e545c
Nikita Belousov <seriouscat1001@gmail.com>
2020-02-27 21:23:19 +0300

    Updates

---
+++ precache-manifest.b05b8027e8c09fd01220c7933c5790c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec5c81213f1e199907912b7b541fb58",
+    "url": "/rps-demo/index.html"
+  },
+  {
+    "revision": "4e325375cdf4f8191808",
+    "url": "/rps-demo/static/css/main.018e352d.chunk.css"
+  },
+  {
+    "revision": "14f9c96d44737534cc2d",
+    "url": "/rps-demo/static/js/2.75d76f59.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/rps-demo/static/js/2.75d76f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e325375cdf4f8191808",
+    "url": "/rps-demo/static/js/main.2688e499.chunk.js"
+  },
+  {
+    "revision": "0b4c5d1254bb1ef34297",
+    "url": "/rps-demo/static/js/runtime-main.a1500b1f.js"
+  },
+  {
+    "revision": "1fd44bdf9274b331fe659004ccc71e63",
+    "url": "/rps-demo/static/media/bg-triangle.1fd44bdf.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/rps-demo/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/rps-demo/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/rps-demo/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "759a207509c8dad61b7e365b27e0d0bd",
+    "url": "/rps-demo/static/media/logo.759a2075.svg"
+  }
+]);
